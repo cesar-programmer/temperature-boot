ce80b80684647352aef6f9d25e3c95f68e846261
Cesar <CesarCordova.developer@gmail.com>
2023-12-12 19:32:47 -0800

    first commit

---
+++ scripts/index.js
@@ -0,0 +1,33 @@
+console.log('Hello world')
+console.table({ name: 'John Doe', age: 30 })
+
+const nameUser = 'cesar'
+const myGrade = 4.8
+const isStudent = true
+
+console.log('Hello ' + nameUser)
+console.log(`Your grade is ${myGrade}`)
+console.log(`Are you a student? ${isStudent}`)
+
+const num = 10
+const num2 = 2
+
+console.log(`Sum: ${num + num2}`)
+console.log('multiplication: ' + (num * num2))
+console.log('division: ' + (num / num2))
+console.log(`subtraction: ${num - num2}`)
+
+const assignmentName = 'Class 1 - Assingment 1'
+const description = 'Assignments are due the day Session 3 is held by 11:59 p.m. (in the time zone in which each student resides). Assignments represent 20% of the overall course grade.'
+const pointsAs1 = 6.66
+const pointsAs2 = 6.66
+const pointsAs3 = 6.66
+
+const total = pointsAs1 + pointsAs2 + pointsAs3
+const fileUploads = true
+const studentAnnotation = false
+
+const dueDate = '12/20/2023'
+
+document.write(`<h1> ${assignmentName} </h1>`)
+document.write(`<p> ${description} </p>`)
